023a6bb6e991972a98b34b6ca597b51e17071b2e
timofey256 <shetilin.timofey@gmail.com>
2025-09-17 10:18:06 +0200

    Add backend README package

---
+++ backend/default.nix
@@ -0,0 +1,52 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  python = pkgs.python313;
+  pyPkgs = python.pkgs;
+in
+pyPkgs.buildPythonPackage {
+  pname = "pytest-smart-debugger-backend";
+  version = "0.0.3";
+  src = pkgs.lib.cleanSource ./.;
+
+  format = "pyproject";
+
+  propagatedBuildInputs = [
+    pyPkgs.setuptools
+    pyPkgs.wheel
+    pyPkgs.black
+    pyPkgs.flake8
+    pyPkgs.isort
+    pyPkgs.mypy
+    pyPkgs.pytest
+    pyPkgs.requests
+    pyPkgs.attrs
+    pyPkgs.referencing
+    pyPkgs.jsonpointer
+    pyPkgs.jsonschema
+    pyPkgs.jsonschema-specifications
+    pyPkgs.jsonpath-ng
+    pyPkgs.arrow
+    pyPkgs.python-dateutil
+    pyPkgs.types-python-dateutil
+    pyPkgs.fqdn
+    pyPkgs.idna
+    pyPkgs.isoduration
+    pyPkgs.jsonschema-specifications
+    pyPkgs.rpds-py
+    pyPkgs.rfc3339-validator
+    pyPkgs.rfc3986-validator
+    pyPkgs.rfc3987
+    pyPkgs.uri-template
+    pyPkgs.webcolors
+    pyPkgs.six
+    pyPkgs.typing-extensions
+    pyPkgs.jsonpickle
+    pyPkgs.build
+    pyPkgs.twine
+
+    pyPkgs.flask # for http backend
+  ];
+
+  doCheck = false;
+}
